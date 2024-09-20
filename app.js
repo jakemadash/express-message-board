0396f49ba33465a5c0014eddf85205e31f7fac39
Jake Ashton <jakemaddenashton@gmail.com>
2024-09-20 14:47:41 -0700

    feat: create message board

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const path = require('node:path')
+
+const app = express()
+
+app.set('views', path.join(__dirname, "views"))
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }));
+
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+  ];
+  
+
+app.get('/', (req, res) => res.render("index", {messages}))
+app.get('/new', (req, res) => res.render("form"))
+app.post('/new', (req, res) => {
+  messages.push({ text: req.body.message, user: req.body.name, added: new Date() })
+  res.redirect("/")
+}
+)
+
+const PORT = 3002
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
